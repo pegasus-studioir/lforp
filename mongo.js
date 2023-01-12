af037a379b5c0bb0b0cd9e092aa5b4e7c91abb7f
Pegasus Studio <86292313+nima-dis@users.noreply.github.com>
2023-01-12 11:19:27 +0330

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,12 @@
+//const mongo = require('mongoose')
+
+//module.exports = async () => {
+    //await mongo.connect("mongodb+srv://lucky:arya7966a@lucky.oznnlrf.mongodb.net/data")
+
+    //return mongo
+//} 
+
+
+//mongo.connection.on('connected', () => {
+    //console.log('connected to Mongo.db')
+//})
